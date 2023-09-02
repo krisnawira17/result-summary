885602f7eb671affc4c6ef999b59b3f779be9246
Krisna Wira Indrawan <krisnawiraindrawan@gmail.com>
2023-09-02 17:38:35 +0700

    finished result summary

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      "Hanken-Grotesk": "sans-serif",
+    },
+    colors: {
+      white: "#FFFFFF",
+      red: "#FF5555",
+      yellow: "#FFB21E",
+      green: "#00BB8F",
+      blue: "#1125D6",
+      "light-purple": "#6943ff",
+      "dark-purple": "#2f2ce9",
+      "dark-navy": "#303B59",
+      "light-blue": "#CAC9FF",
+      "very-light-blue": "#ECF2FF",
+      "gradient2-purple-dark": "#4D21C9",
+      "gradient2-purple-light": "#2521C9",
+    },
+  },
+  plugins: [],
+};
